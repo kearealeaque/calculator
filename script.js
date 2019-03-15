a1593f5e9dfd922b45cc455feb95e6e1046f679e
k.naumov <k.naumov@xsolla.com>
2019-03-15 23:47:39 +0500

    c:

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-"use strict";
-
-var input = document.getElementById('input'),
-input.addEventListener('keyup', function(event) {
-  if (event.keyCode === 13) {
-        event.preventDefault();
-  document.getElementById('equals').click();
-  }
-});
-
+var display = document.getElementById('display');
+var nums = document.getElementsByClassName('num');
+var ops = document.getElementsByClassName('ops');
+var equals = document.getElementById('equals');
 var erase = document.getElementById('erase');
-erase.addEventListener('keyup', function(event) {
-  input = "";
-});
+
+for (var i = 0; i < nums.length; i++) {
+    nums[i].addEventListener('click', buttonClick, false);
+}
+
+for (var i = 0; i < ops.length; i++) {
+    ops[i].addEventListener('click', buttonClick, false);
+}
+
+function buttonClick() {
+    var input = document.getElementById('screen');
+    var inputVal = input.innerHTML;
+    var btnVal = this.innerHTML;
+
+    if(btnVal == 'CE') {
+			input.innerHTML = '';
+		}
+
+    else if(btnVal == '=') {
+			var equation = inputVal;
+      equation = equation.replace(/x/g, '*').replace(/÷/g, '/').replace(/\^/g, '\*\*');
+
+      if(equation)
+  input.innerHTML = eval(equation);
+  decimalAdded = false;
+}
